58c38fc667ba7449336848343e6ba8fe1c408f19
Douglas Henrique <dev.dhbarboza@gmail.com>
2022-11-22 03:52:13 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://8080-dhbarboza-springlab-rktl4470q54.ws-us77.gitpod.io'
+axios.defaults.baseURL = 'https://8080-dhbarboza-springlab-4r358uzvq3g.ws-us77.gitpod.io'
 
 axios.interceptors.request.use(config => {
   if(store.state.token) {
